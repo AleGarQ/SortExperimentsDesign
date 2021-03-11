ed91b5de2d962f7c421738af30f3da1163c8a24a
Alejandro <alejo.gar.122@gmail.com>
2021-03-11 17:57:50 -0500

    Insertion sort added

---
+++ ExperimentsDesign/model/InsertionSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ExperimentsDesign
+{
+    public class InsertionSort
+    {
+        public InsertionSort() 
+        { 
+            
+        }
+
+        public void sort(long[] arr) 
+        {
+            long n = arr.Length;
+            for (long i = 1; i < n; ++i)
+            {
+                long key = arr[i];
+                long j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+
+        }
+    }
+}
